1fd1277d68895abf2520d2f65656af1a048e8ec9
kundurusunitha26 <kundurusunitha085@gmail.com>
2026-09-11 12:16:09 +0530

    Initial commit

---
+++ src/Demo/InsertAtBeginning.java
@@ -0,0 +1,34 @@
+package Demo;
+
+
+public class InsertAtBeginning {
+	static class Node {
+		int data;
+		Node next;
+		Node(int data) {
+			this.data=data;
+		}
+	}
+	Node head=null;
+	void insertAtBeginning(int data) {
+		Node newNode=new Node(data);
+		newNode.next=head;
+		head=newNode;
+	}
+	void display() {
+		Node current=head;
+		while(current!=null) {
+			System.out.print(current.data+" -> ");
+			current=current.next;
+		}
+		System.out.println("null");
+	}
+	public static void main(String[] args) {
+		InsertAtBeginning list=new InsertAtBeginning();
+		list.insertAtBeginning(30);
+		list.insertAtBeginning(20);
+		list.insertAtBeginning(10);
+		list.insertAtBeginning(5);
+		list.display();
+	}
+}
